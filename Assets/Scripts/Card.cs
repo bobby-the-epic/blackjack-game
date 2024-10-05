263aacaffd2c3ff0b1eac00848bf10d13853dbc8
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-10-05 16:11:59 -0700

    Add point value functionality The card script will decide how many points a card is worth. Aces are assigned 0 points so that the Main script can determine how many points they are worth based on the player's/dealer's current hand.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    public int pointValue { get; private set; } = 0;
     public GameObject cardFront;
     public SpriteRenderer spriteRenderer;
+
+    bool isAce = false;
 
     void Awake()
     {
@@ -13,7 +16,28 @@ public class Card : MonoBehaviour
     }
     public void SetCard(Sprite sprite)
     {
+        /*
+            Renames the gameobject to the name of the sprite texture, removes the prefix
+            of the name (the prefix is the suit), and sets the point value of the card.
+        */
+
         spriteRenderer.sprite = sprite;
         name = sprite.name;
+        string cardNum = name.Remove(0, 1);
+        int num = int.Parse(cardNum);
+
+        switch (num)
+        {
+            case 1:
+                isAce = true;
+                pointValue = 0;
+                break;
+            case > 10:
+                pointValue = 10;
+                break;
+            default:
+                pointValue = num;
+                break;
+        }
     }
 }
